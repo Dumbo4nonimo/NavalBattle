ab8243b2b9a25c3a22c779d7670ad419a679e3a6
dylan <dylanfer830@gmail.com>
2024-05-23 19:59:49 -0500

    Avance en tablero 2d V 1.1

---
+++ src/main/java/org/example/navalbattle/model/Ship.java
@@ -0,0 +1,31 @@
+package org.example.navalbattle.model;
+
+public class Ship {
+    private int x;
+    private int y;
+    private int length;
+    private boolean isHorizontal;
+
+    public Ship(int x, int y, int length, boolean isHorizontal) {
+        this.x = x;
+        this.y = y;
+        this.length = length;
+        this.isHorizontal = isHorizontal;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+}
