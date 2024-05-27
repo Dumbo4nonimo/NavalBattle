acb403247363101af34aaa576473dc443d931aa9
dylan <dylanfer830@gmail.com>
2024-05-27 17:33:49 -0500

    Correcion de errores 1.0

--- src/main/java/org/example/navalbattle/model/BattleShipBoard.java
+++ src/main/java/org/example/navalbattle/model/BattleShipBoard.java
@@ -2,77 +2,94 @@ package org.example.navalbattle.model;
 
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
+/**
+ * Represents the game board for the battleship game.
+ */
 public class BattleShipBoard {
 
     private GridPane shipGridPane;
     private int size;
     private Ship[] ships;
 
+    /**
+     * Constructs a BattleShipBoard object.
+     *
+     * @param shipGridPane the GridPane to place the ships on
+     * @param size         the size of the board
+     */
     public BattleShipBoard(GridPane shipGridPane, int size) {
         this.shipGridPane = shipGridPane;
         this.size = size;
-
     }
+
+    /**
+     * Initializes the board layout and the ships.
+     */
     public void initializeBoard() {
-        shipGridPane.setLayoutX(1000); // Move 50 pixels to the right
-        shipGridPane.setLayoutY(1000);
-        initializeGrid();// Inicializar el grid
-        initializeShips(); // Inicializar los barcos
+        shipGridPane.setLayoutX(30); // Move 30 pixels to the right
+        shipGridPane.setLayoutY(30);
+        initializeShips(); // Initialize the ships
     }
 
+    /**
+     * Initializes the ships and places them on the board.
+     */
     private void initializeShips() {
-        ships = new Ship[] {
-                new Ship(0, 0, 3, true, 0),  // Barco de longitud 3 en posición (0, 0)
-                new Ship(0, 0, 3, true, 0),  // Barco de longitud 3 en posición (0, 0)
-                new Ship(5, 0, 4, true, 1),// Barco de longitud 4 en posición (5, 0)
-                new Ship(0,2, 2, true, 2 ), // Barco de longitud 2 en posición (0, 2)
-                new Ship(0,2, 2, true, 2 ), // Barco de longitud 2 en posición (0, 2)
-                new Ship(0,2, 2, true, 2 ), // Barco de longitud 2 en posición (0, 2)
-                new Ship(5,2, 1, true, 3 ), // Barco de longitud 1 en posición (5, 2)
-                new Ship(5,2, 1, true, 3 ), // Barco de longitud 1 en posición (5, 2)
-                new Ship(5,2, 1, true, 3 ), // Barco de longitud 1 en posición (5, 2)
-                new Ship(5,2, 1, true, 3 ) // Barco de longitud 1 en posición (5, 2)
+        ships = new Ship[]{
+                new Ship(0, 0, 3, false, 0),  // Ship of length 3 at position (0, 0)
+                new Ship(0, 0, 3, true, 0),   // Ship of length 3 at position (0, 0)
+                new Ship(5, 0, 4, true, 1),   // Ship of length 4 at position (5, 0)
+                new Ship(0, 2, 2, true, 2),   // Ship of length 2 at position (0, 2)
+                new Ship(0, 2, 2, true, 2),   // Ship of length 2 at position (0, 2)
+                new Ship(0, 2, 2, true, 2),   // Ship of length 2 at position (0, 2)
+                new Ship(5, 2, 1, true, 3),   // Ship of length 1 at position (5, 2)
+                new Ship(5, 2, 1, true, 3),   // Ship of length 1 at position (5, 2)
+                new Ship(5, 2, 1, true, 3),   // Ship of length 1 at position (5, 2)
+                new Ship(5, 2, 1, true, 3)    // Ship of length 1 at position (5, 2)
         };
-        for (int i = 0; i < ships.length ; i++) {
-            drawShip(ships[i], ships[i].getType());
+        for (Ship ship : ships) {
+            drawShip(ship, ship.getType());
         }
     }
 
-    private void drawShip(Ship ship, int shipType) {
+    /**
+     * Draws a ship on the board.
+     *
+     * @param ship     the ship to be drawn
+     * @param shipType the type of the ship
+     */
+    public void drawShip(Ship ship, int shipType) {
         int cellSize = 30;
         int x = ship.getX() * cellSize;
         int y = ship.getY() * cellSize;
         int width = ship.isHorizontal() ? ship.getLength() * cellSize : cellSize;
         int height = ship.isHorizontal() ? cellSize : ship.getLength() * cellSize;
 
-        // Calcular los índices de fila y columna
+        // Calculate row and column indices
         int row = ship.getY();
         int col = ship.getX();
 
-        DraggableShip draggableShip = new DraggableShip(x, y, width, height, shipGridPane, shipType);
-        draggableShip.setFill(Color.GRAY);
+        DraggableShip draggableShip = new DraggableShip(x, y, width, height, shipGridPane, ship);
 
-        // Agregar el barco al GridPane con colspan y rowspan para superponerlo
+        // Add the ship to the GridPane with colspan and rowspan to span multiple cells
         shipGridPane.add(draggableShip, col, row, ship.isHorizontal() ? ship.getLength() : 1, ship.isHorizontal() ? 1 : ship.getLength());
-
     }
 
-    // Método para inicializar el grid con celdas azules
-    public void initializeGrid() {
-        for (int row = 8; row < size+8; row++) {
-            for (int col = 0; col < size; col++) {
-                Rectangle cell = new Rectangle(30, 30);
-                cell.setFill(Color.BLUE);
-                cell.setStroke(Color.BLACK);
-                shipGridPane.add(cell, col, row);
-            }
-        }
-    }
+    /**
+     * Gets the array of ships on the board.
+     *
+     * @return the array of ships
+     */
     public Ship[] getShips() {
-        return ships;}
+        return ships;
+    }
 
+    /**
+     * Gets the GridPane of the board.
+     *
+     * @return the GridPane
+     */
     public GridPane getGridPane() {
         return shipGridPane;
     }
--- src/main/java/org/example/navalbattle/model/Game.java
+++ src/main/java/org/example/navalbattle/model/Game.java
@@ -2,64 +2,65 @@ package org.example.navalbattle.model;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import org.example.navalbattle.view.GameStage;
 
 import java.io.IOException;
 
+/**
+ * Represents the game logic and UI for the naval battle game.
+ */
 public class Game implements IGame {
 
     @FXML
-    private GridPane gridPane, shipGridPane;
-    private int size; // battlefield size, rows and columns
-    private char[][] board; // register
+    private GridPane gridPane, shipGridPane, gridImages;
+    private int size;
+    private char[][] board;
     private BattleShipBoard battleShipBoard;
 
-    public void initializeBoard(int tablero) throws IOException {
-        try {
-            shipGridPane = new GridPane();
-            shipGridPane.setLayoutX(1000); // Adjust as needed
-            shipGridPane.setLayoutY(1000); // Adjust as needed
-
-            // Inicializar el shipGridPane con celdas visibles
-            for (int row = 0; row < size; row++) {
-                for (int col = 0; col < size; col++) {
-                    Rectangle cell = new Rectangle(30, 30);
-                    cell.setFill(Color.LIGHTBLUE);
-                    cell.setStroke(Color.BLACK);
-                    shipGridPane.add(cell, col, row);
-                }
+    /**
+     * Creates a grid of images on the game board.
+     */
+    public void createGridImages() {
+        gridImages = new GridPane(); // Initialize the GridPane
+        gridPane.setAlignment(Pos.TOP_RIGHT);
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                // Add images to the gridImages
+                Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/loginShip.jpg"));
+                ImageView imageView = new ImageView(img);
+                imageView.setFitWidth(20);
+                imageView.setFitHeight(20);
+                gridImages.add(imageView, col, row);
             }
+        }
+    }
 
-            // Ahora puedes inicializar BattleShipBoard
+    /**
+     * Initializes the game board and ships based on the given tablero type.
+     *
+     * @param tablero the type of board to initialize
+     * @throws IOException if an error occurs during initialization
+     */
+    public void initializeBoard(int tablero) throws IOException {
+        createGridImages(); // Calls the method to create the GridPane of images
+
+        try {
             battleShipBoard = new BattleShipBoard(shipGridPane, size);
-            battleShipBoard.initializeBoard(); // Inicializar el tablero de barcos
+            battleShipBoard.initializeBoard(); // Initializes the ship board
 
             if (tablero == 0) {
-                board = new char[size][size];
                 gridPane.setAlignment(Pos.CENTER);
-                Image img = new Image(getClass().getResource("/org/example/navalbattle/images/lose.png").toString());
-
-                ImageView imageView = new ImageView(img);
-                imageView.setFitWidth(35);
-                imageView.setFitHeight(35);
-
                 for (int row = 0; row < size; row++) {
                     for (int col = 0; col < size; col++) {
-                        board[row][col] = ' '; // Position without ships
-
+                        // Position without ships
                         Button button = new Button();
                         button.setPrefWidth(35);
                         button.setPrefHeight(35);
-
                         gridPane.add(button, col, row);
-
                         int finalRow = row;
                         int finalCol = col;
                         button.setOnAction(event -> handleButtonClick(finalRow, finalCol));
@@ -68,12 +69,6 @@ public class Game implements IGame {
             } else if (tablero == 1) {
                 board = new char[size][size];
                 gridPane.setAlignment(Pos.CENTER);
-                Image img = new Image(getClass().getResource("/org/example/navalbattle/images/lose.png").toString());
-
-                ImageView imageView = new ImageView(img);
-                imageView.setFitWidth(35);
-                imageView.setFitHeight(35);
-
                 for (int row = 0; row < size; row++) {
                     for (int col = 0; col < size; col++) {
                         board[row][col] = ' '; // Position without ships
@@ -81,7 +76,6 @@ public class Game implements IGame {
                         Button button = new Button();
                         button.setPrefWidth(35);
                         button.setPrefHeight(35);
-
                         gridPane.add(button, col, row);
 
                         int finalRow = row;
@@ -91,89 +85,112 @@ public class Game implements IGame {
                 }
             }
         } catch (Exception e) {
+            e.printStackTrace(); // Print full error trace
             System.err.println("Error loading the image: " + e.getMessage());
         }
     }
 
+    /**
+     * Handles the button click event for the non-ship grid.
+     *
+     * @param finalRow the row of the clicked button
+     * @param finalCol the column of the clicked button
+     */
     private void handleButtonClick(int finalRow, int finalCol) {
         try {
             GameStage gameStage = GameStage.getInstance();
-
             Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/lose.png"));
-
             ImageView imageView = new ImageView(img);
             imageView.setFitWidth(20);
             imageView.setFitHeight(20);
-
-            Button clickedButton = (Button) gridPane.getChildren().get(finalRow * size + finalCol);
-
+            Button clickedButton = (Button) gridPane.getChildren().get((finalRow * size + 1) + finalCol);
             clickedButton.setGraphic(imageView);
             System.out.println("The position is: row " + (finalRow + 1) + " col " + (finalCol + 1));
             attack(finalRow, finalCol, false);
 
-            gameStage.switchToScene2();
-
             clickedButton.setDisable(true);
-
         } catch (Exception e) {
             System.err.println("Error handling the button click: " + e.getMessage());
         }
     }
 
+    /**
+     * Sets the GridPane for the game.
+     *
+     * @param gridPane the GridPane to set
+     */
     public void setGridPane(GridPane gridPane) {
         this.gridPane = gridPane;
     }
 
+    /**
+     * Sets the GridPane for the ships.
+     *
+     * @param shipGridPane the ship GridPane to set
+     */
+    public void setShipGridPane(GridPane shipGridPane) {
+        this.shipGridPane = shipGridPane;
+    }
+
+    /**
+     * Sets the size of the game board.
+     *
+     * @param size the size to set
+     */
     public void setSize(int size) {
         this.size = size;
     }
 
+    /**
+     * Handles the button click event for the ship grid.
+     *
+     * @param finalRow the row of the clicked button
+     * @param finalCol the column of the clicked button
+     */
     private void handleButtonClick1(int finalRow, int finalCol) {
         try {
             GameStage gameStage = GameStage.getInstance();
-
             Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/lose.png"));
-
             ImageView imageView = new ImageView(img);
             imageView.setFitWidth(20);
             imageView.setFitHeight(20);
-
             Button clickedButton = (Button) gridPane.getChildren().get(finalRow * size + finalCol);
-
             clickedButton.setGraphic(imageView);
             System.out.println("The position is: row " + (finalRow + 1) + " col " + (finalCol + 1));
             attack(finalRow, finalCol, true);
-
             gameStage.switchToScene1();
-
             clickedButton.setDisable(true);
-
         } catch (Exception e) {
             System.err.println("Error handling the button click: " + e.getMessage());
         }
     }
 
+    /**
+     * Performs an attack on the specified position.
+     *
+     * @param row the row to attack
+     * @param col the column to attack
+     * @param hit true if the attack is a hit, false otherwise
+     */
     public void attack(int row, int col, boolean hit) {
         try {
-            String imagePath = hit ? "/org/example/navalbattle/images/bomb.png" : "/org/example/navalbattle/images/lose.png";
+            String imagePath = hit ? "/org/example/navalbattle/images/bomb.png" : "/org/example/navalbattle/images/loginShip.jpg";
             Image img = new Image(getClass().getResourceAsStream(imagePath));
-
             ImageView imageView = new ImageView(img);
-            imageView.setFitWidth(30);
-            imageView.setFitHeight(30);
-
-            // Limpia el GridPane antes de agregar una nueva imagen
-            battleShipBoard.getGridPane().getChildren().removeIf(node -> GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == col);
-
-            // Agrega la nueva imagen al GridPane
+            imageView.setFitWidth(29);
+            imageView.setFitHeight(29);
             battleShipBoard.getGridPane().add(imageView, col, row);
-            System.out.println("Pa el boton si se ejecuta");
-
+            System.out.println("Button executed");
         } catch (Exception e) {
             System.err.println("Error handling the attack: " + e.getMessage());
         }
     }
 
+    /**
+     * Sets the BattleShipBoard for the game.
+     *
+     * @param battleShipBoard the BattleShipBoard to set
+     */
     public void setBattleShipBoard(BattleShipBoard battleShipBoard) {
         this.battleShipBoard = battleShipBoard;
     }
