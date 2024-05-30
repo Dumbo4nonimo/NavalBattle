a6d7bc77b8954ec7e6c72a9b406d437ad76aa17b
dylan <dylanfer830@gmail.com>
2024-05-30 15:59:35 -0500

    Attack Update

--- src/main/java/org/example/navalbattle/model/Game.java
+++ src/main/java/org/example/navalbattle/model/Game.java
@@ -3,6 +3,7 @@ package org.example.navalbattle.model;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -20,9 +21,25 @@ public class Game implements IGame {
     @FXML
     private GridPane gridPane, shipGridPane,grid2;
     private int size;
-    private char[][] board;
+
     private BattleShipBoard battleShipBoard;
     private Button startBattleButton,winTheGame;
+    private static final String[][] board = {
+            {"", "1", "", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", "", "1", ""},
+            {"", "", "", "", "", "", "", "", "1", ""},
+            {"1", "", "1", "1", "1", "", "", "1", "", ""},
+            {"", "", "", "", "", "", "1", "", "", ""},
+            {"", "", "", "", "", "", "1", "", "", ""},
+            {"", "", "", "1", "1", "1", "1", "", "", "1"},
+            {"", "", "", "", "", "", "", "", "", ""},
+            {"", "1", "", "", "1", "", "", "", "1", ""},
+            {"", "1", "", "", "", "", "", "", "1", ""},
+    };
+
+    public static String[][] getBoard() {
+        return board;
+    }
 
     /**
      * Creates a grid of images on the game board.
@@ -53,27 +70,8 @@ public class Game implements IGame {
                         button.setOnAction(event -> handleButtonClick(finalRow, finalCol));
                     }
                 }
-            } else if (tablero == 1) {
-                board = new char[size][size];
-                gridPane.setAlignment(Pos.CENTER);
-                for (int row = 0; row < size; row++) {
-                    for (int col = 0; col < size; col++) {
-                        board[row][col] = ' '; // Position without ships
-
-                        Button button = new Button();
-                        button.setPrefWidth(35);
-                        button.setPrefHeight(35);
-                        gridPane.add(button, col, row);
-                        Random random = new Random();
-                        int randomHitCol = random.nextInt(11);
-                        int randomHitRow = random.nextInt(11);
-                        int finalRow = row;
-                        int finalCol = col;
-                        button.setOnAction(event -> handleButtonClick1(finalRow, finalCol));
-
-                    }
-                }
             }
+
         } catch (Exception e) {
             e.printStackTrace(); // Print full error trace
             System.err.println("Error loading the image: " + e.getMessage());
@@ -88,20 +86,47 @@ public class Game implements IGame {
      */
     private void handleButtonClick(int finalRow, int finalCol) {
         try {
+            boolean hasShip = board[finalRow][finalCol].isEmpty();
+
+
             GameStage gameStage = GameStage.getInstance();
-            Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/lose.png"));
-            ImageView imageView = new ImageView(img);
-            imageView.setFitWidth(20);
-            imageView.setFitHeight(20);
-            Button clickedButton = (Button) gridPane.getChildren().get((finalRow * size + 1) + finalCol);
-            clickedButton.setGraphic(imageView);
-            System.out.println("The position is: row " + (finalRow + 1) + " col " + (finalCol + 1));
-            int hitCol,hitRow;
-            Random random = new Random();
-            hitRow = random.nextInt(11);
-            hitCol = random.nextInt(11);
-            attack(hitCol, hitRow, false);
-            clickedButton.setDisable(true);
+            if(hasShip){
+                Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/lose.jpg"));
+                ImageView imageView = new ImageView(img);
+                imageView.setFitWidth(20);
+                imageView.setFitHeight(20);
+                Button clickedButton = (Button) gridPane.getChildren().get((finalRow * size + 1) + finalCol);
+                clickedButton.setGraphic(imageView);
+                clickedButton.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                clickedButton.setGraphicTextGap(0);
+                clickedButton.setAlignment(Pos.CENTER);
+
+                System.out.println("The position is: row " + (finalRow + 1) + " col " + (finalCol + 1));
+                int hitCol,hitRow;
+                Random random = new Random();
+                hitRow = random.nextInt(11);
+                hitCol =random.nextInt(3,11);
+                attack(hitCol, hitRow, false);
+                clickedButton.setDisable(true);
+            }
+            else{
+                Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/loginShip.jpg"));
+                ImageView imageView = new ImageView(img);
+                imageView.setFitWidth(20);
+                imageView.setFitHeight(20);
+                Button clickedButton = (Button) gridPane.getChildren().get((finalRow * size + 1) + finalCol);
+                clickedButton.setGraphic(imageView);
+                clickedButton.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                clickedButton.setGraphicTextGap(0);
+                clickedButton.setAlignment(Pos.CENTER);
+                System.out.println("The position is: row " + (finalRow + 1) + " col " + (finalCol + 1));
+                int hitCol,hitRow;
+                Random random = new Random();
+                hitRow =random.nextInt(11);
+                hitCol = random.nextInt(3,11);
+                attack(hitCol, hitRow, false);
+                clickedButton.setDisable(true);}
+
         } catch (Exception e) {
             System.err.println("Error handling the button click: " + e.getMessage());
         }
@@ -142,29 +167,6 @@ public class Game implements IGame {
         return startBattleButton;
     }
 
-    /**
-     * Handles the button click event for the ship grid.
-     *
-     * @param finalRow the row of the clicked button
-     * @param finalCol the column of the clicked button
-     */
-    private void handleButtonClick1(int finalRow, int finalCol) {
-        try {
-            GameStage gameStage = GameStage.getInstance();
-            Image img = new Image(getClass().getResourceAsStream("/org/example/navalbattle/images/lose.png"));
-            ImageView imageView = new ImageView(img);
-            imageView.setFitWidth(20);
-            imageView.setFitHeight(20);
-            Button clickedButton = (Button) gridPane.getChildren().get(finalRow * size + finalCol);
-            clickedButton.setGraphic(imageView);
-            System.out.println("The position is: row " + (finalRow + 1) + " col " + (finalCol + 1));
-            attack(finalRow, finalCol, true);
-            gameStage.switchToScene1();
-            clickedButton.setDisable(true);
-        } catch (Exception e) {
-            System.err.println("Error handling the button click: " + e.getMessage());
-        }
-    }
 
     /**
      * Performs an attack on the specified position.
@@ -173,9 +175,11 @@ public class Game implements IGame {
      * @param col the column to attack
      * @param hit true if the attack is a hit, false otherwise
      */
+
     public void attack(int row, int col, boolean hit) {
         try {
-            String imagePath = hit ? "/org/example/navalbattle/images/bomb.png" : "/org/example/navalbattle/images/loginShip.jpg";
+
+            String imagePath = hit ? "/org/example/navalbattle/images/lose.jpg" : "/org/example/navalbattle/images/loginShip.jpg";
             Image img = new Image(getClass().getResourceAsStream(imagePath));
             ImageView imageView = new ImageView(img);
             imageView.setFitWidth(29);
@@ -195,4 +199,6 @@ public class Game implements IGame {
     public void setBattleShipBoard(BattleShipBoard battleShipBoard) {
         this.battleShipBoard = battleShipBoard;
     }
+
+
 }
