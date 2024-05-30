cc36aa9a7e9bc6ed2c9ae5838ab64618d1ebef5f
NickCoder <NickCoder1309@users.noreply.github.com>
2024-05-30 11:35:21 -0500

    Creation of All Ships Restrictions for each type of ship Color for each ship

--- src/main/java/org/example/navalbattle/controller/GameNavalBattleController1.java
+++ src/main/java/org/example/navalbattle/controller/GameNavalBattleController1.java
@@ -1,6 +1,7 @@
 package org.example.navalbattle.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import org.example.navalbattle.model.BattleShipBoard;
 import org.example.navalbattle.model.Game;
@@ -24,7 +25,7 @@ import java.io.IOException;
                 game1.setBattleShipBoard(shipBoard1);
                 game1.setGridPane(gridPane1);
                 game1.setShipGridPane(shipGridPane1); // Pasa el shipGridPane a la clase Game
-                game1.initializeBoard(0);
+                //game1.initializeBoard(0, );
             }
             catch (Exception e) {
                 System.err.println("Error handling the button click: " + e.getMessage());
